0e80a9cf97c2c5aa31a7b0b053d0674ba861e375
AlessandroPavan01 <91598666+AlessandroPavan01@users.noreply.github.com>
2021-12-11 19:26:55 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,11 @@
+//
+// Created by Alessandro on 11/12/2021.
+//
+
+#ifndef ESERCITAZIONE9_LIB_H
+#define ESERCITAZIONE9_LIB_H
+
+bool ricerca(char lista[10][20], char nome [1][20], int &pos);
+
+#endif //ESERCITAZIONE9_LIB_H
 
