bcc558783f8689dbfb208d06b209523d43d47687
AlessandroPavan01 <91598666+AlessandroPavan01@users.noreply.github.com>
2021-12-11 19:35:59 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,12 +1,11 @@
-//
-// Created by Alessandro on 11/12/2021.
-//
+
 #include "lib.h"
 #include <cstring>
 
 bool ricerca( char lista [10][20], char nome[1][20], int &pos){
+    
     for(int i=0; i<10; i++){
-        if(strcmp (lista [i], nome [o]) == 0){
+        if(strcmp (lista [i], nome [0]) == 0){
             pos = i;
             return true;
         }
